036eea1a8fc8f70b25707ed4544b8853149f5bc6
帅得乱七八糟 <284970670@qq.com>
2021-07-24 21:41:37 +0800

    3-13 获取网络API数据 可以看看app.tsx文件的函数组件和类组件转化

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -4,21 +4,50 @@ import styles from './App.module.css';
 import robots from './mockdata/robots.json';  // 似乎还不能把mockdata文件夹放在src文件夹外，不然还访问不到
 import Robot from './components/Robot';
 import ShoppingCart from './components/ShoppingCart';
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="react-logo" />
-        <h1>罗伯特吊炸天机器人</h1>
+
+interface Props {
+
+}
+
+interface State {
+  robotGallery: any[]
+}
+
+// 一下子就将函数组件转变为类组件了！(记录一下，有哪些变化)
+class App extends React.Component<Props, State> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      robotGallery: []
+    }
+  }
+
+  componentDidMount() {
+    // fetch方法，返回的是Promise对象
+    fetch('https://jsonplaceholder.typicode.com/users').
+    then(response => response.json()).      // 这里的json()是用来获取json数据的
+    then(data => this.setState({
+      robotGallery: data
+    }))
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="react-logo" />
+          <h1>罗伯特吊炸天机器人</h1>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map(robot => {
+            return <Robot id={robot.id} email={robot.email} name={robot.name}/>
+          })}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robots.map(robot => {
-          return <Robot id={robot.id} email={robot.email} name={robot.name}/>
-        })}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
