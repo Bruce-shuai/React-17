8fe91893d0c6e6d116737320251f80d390a24ea9
帅得乱七八糟 <284970670@qq.com>
2021-07-25 21:07:11 +0800

    4-6 在useEffect里面使用了async……await

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -4,22 +4,39 @@ import styles from './App.module.css';
 import robots from './mockdata/robots.json';  // 似乎还不能把mockdata文件夹放在src文件夹外，不然还访问不到
 import Robot from './components/Robot';
 import ShoppingCart from './components/ShoppingCart';
-
 // 一下子就将函数组件转变为类组件了！(记录一下，有哪些变化)
 const App:React.FC = () => {
 
   // 这是数组解构的方法
   const [count, setCount] = useState<number>(0);
   const [robotGallery, setRobotGallery] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
   useEffect(() => {
     document.title = `已点击${count}次`;
   }, [count])
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users').
-    then(response => response.json()).
-    then(data => setRobotGallery(data));
+    // 数据还未请求的时候，处于加载状态
+    setLoading(false);
+    // 这样在useEffect里面使用 async……await
+    // try……catch 是在async内部使用
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();  
+        setRobotGallery(data);
+      } catch(e) {
+        setError(e.message);
+      }
+      // 获取数据成功后的状态
+      setLoading(true);
+    }
+    fetchData();
   }, [])
+
+
   return (
     <div className={styles.app}>
       <div className={styles.appHeader}>
@@ -32,11 +49,20 @@ const App:React.FC = () => {
         <span>count: {count}</span>
       </button>
       <ShoppingCart />
-      <div className={styles.robotList}>
-        {robotGallery.map(robot => {
-          return <Robot id={robot.id} email={robot.email} name={robot.name}/>
-        })}
-      </div>
+      {
+        (!error || error !== '') && <div>网站错误：{error}</div>
+        // !error || <div>网站错误：{error}</div>  // 尽管这个也是可以运行的，但是比上面的还是要差一些
+      }
+      {
+        loading ? 
+        <div className={styles.robotList}>
+          {robotGallery.map(robot => {
+            return <Robot id={robot.id} email={robot.email} name={robot.name}/>
+          })}
+        </div>
+        :
+        <div> Loading ...<img src={logo} /></div> 
+      }   
     </div>
   );
 }
