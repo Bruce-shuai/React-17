020f8a19c7a21acdd7942282a937e154435c09c9
帅得乱七八糟 <284970670@qq.com>
2021-07-25 18:47:00 +0800

    4-6 对于useEffect 有了一定的理解

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/imgs/logo.svg';
 import styles from './App.module.css';
 import robots from './mockdata/robots.json';  // 似乎还不能把mockdata文件夹放在src文件夹外，不然还访问不到
@@ -6,10 +6,20 @@ import Robot from './components/Robot';
 import ShoppingCart from './components/ShoppingCart';
 
 // 一下子就将函数组件转变为类组件了！(记录一下，有哪些变化)
-const App = () => {
+const App:React.FC = () => {
 
+  // 这是数组解构的方法
   const [count, setCount] = useState<number>(0);
+  const [robotGallery, setRobotGallery] = useState<any>([]);
+  useEffect(() => {
+    document.title = `已点击${count}次`;
+  }, [count])
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users').
+    then(response => response.json()).
+    then(data => setRobotGallery(data));
+  }, [])
   return (
     <div className={styles.app}>
       <div className={styles.appHeader}>
@@ -22,11 +32,11 @@ const App = () => {
         <span>count: {count}</span>
       </button>
       <ShoppingCart />
-      {/* <div className={styles.robotList}>
-        {this.state.robotGallery.map(robot => {
+      <div className={styles.robotList}>
+        {robotGallery.map(robot => {
           return <Robot id={robot.id} email={robot.email} name={robot.name}/>
         })}
-      </div> */}
+      </div>
     </div>
   );
 }
