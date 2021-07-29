f0fbaea3a203e7717edef9346ab4c8d970f6759c
帅得乱七八糟 <284970670@qq.com>
2021-07-29 16:22:13 +0800

    6-7 Link组件的使用

--- react-travel/src/components/product-collection/ProductImage.tsx
+++ react-travel/src/components/product-collection/ProductImage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, Typography } from 'antd';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { withRouter, RouteComponentProps, Link } from 'react-router-dom';
+
 import styles from './ProductCollection.module.css';
 // 在ts中要使用withRouter，参数类型需要继承RouteComponentProps类型定义
 interface Props extends RouteComponentProps{
@@ -18,7 +19,7 @@ interface Props extends RouteComponentProps{
 }
 
 const ProductImageComponent:React.FC<Props> = ({size, product, history}) => {
-  return <div className={styles.product} onClick={() => history.push(`detail/${product.id}`)}>
+  return <Link className={styles.product} to={`detail/${product.id}`}>
     {
       size === 'big' ? 
       <Image className={styles['big-img']} src={product.touristRoutePictures[0].url}/> : 
@@ -28,7 +29,7 @@ const ProductImageComponent:React.FC<Props> = ({size, product, history}) => {
     <Typography.Text>{product.title.slice(0, 15)}</Typography.Text>
     <Typography.Text type='danger'>{`￥${product.price}起`}</Typography.Text>
     </div>
-  </div>
+  </Link>
 }
 
 export const ProductImage = withRouter(ProductImageComponent);
