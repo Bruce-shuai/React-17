cabfa71a5047b5cf60f1851c53819a7ad1bb4bfd
帅得乱七八糟 <284970670@qq.com>
2021-07-29 10:24:08 +0800

    6-4路由基础知识

--- react-travel/src/App.tsx
+++ react-travel/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './App.module.css';
 import { HomePage } from './pages';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App:React.FC = () => {
 
   return (
     <div className={styles.App}>
       <BrowserRouter>
-        <Route path='/' component={HomePage}></Route>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/sign' render={() => <div>用户登录页面</div>} />
+          <Route render={() => <div>404 not found 页面去火星了</div>} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
