261ed43100fb7fb39bdd2ebc5525659c5171f660
帅得乱七八糟 <284970670@qq.com>
2021-08-03 11:44:02 +0800

    10-3 使用antd + ts 来处理表单

---
+++ react-travel/src/pages/register/RegisterForm.tsx
@@ -0,0 +1,73 @@
+// 专门用一个组件来实现 表单注册
+import { Form, Input, Button, Checkbox } from 'antd';
+
+export const RegisterForm = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      {/* 密码确认功能  挺有意思的 直接使用antd 的模板*/}
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('The two passwords that you entered do not match!'));
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
--- react-travel/src/pages/register/RegisterPage.tsx
+++ react-travel/src/pages/register/RegisterPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { UserLayout } from '../../layouts/userLayout';
+import { RegisterForm } from './RegisterForm'; 
 export const RegisterPage: React.FC = (props) => {
   console.log(props);
   
   return <UserLayout>
-    注册页面
+    <RegisterForm />
   </UserLayout>
 }
